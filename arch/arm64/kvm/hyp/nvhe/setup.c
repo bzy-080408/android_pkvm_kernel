8f90d9ef77a6ada449579a5333a3fe5c0c5a492a
Will Deacon <will@kernel.org>
2019-12-05 19:48:48 +0000

    KVM: arm64: Add debug UART hacks at EL2
    Provide a standalone mechanism for debug UART access at EL2. It's a
    debug hack, so please try not to get too offended by it.
    
    [ qperret: re-map UART at EL2 in protected mode ]
    
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/kvm/hyp/nvhe/setup.c
+++ arch/arm64/kvm/hyp/nvhe/setup.c
@@ -23,6 +23,25 @@ unsigned long hyp_nr_cpus;
 #define hyp_percpu_size ((unsigned long)__per_cpu_end - \
 			 (unsigned long)__per_cpu_start)
 
+#ifdef CONFIG_KVM_ARM_HYP_DEBUG_UART
+unsigned long arm64_kvm_hyp_debug_uart_addr;
+static int create_hyp_debug_uart_mapping(void)
+{
+	phys_addr_t base = CONFIG_KVM_ARM_HYP_DEBUG_UART_ADDR;
+	unsigned long haddr;
+
+	haddr = __pkvm_create_private_mapping(base, PAGE_SIZE, PAGE_HYP_DEVICE);
+	if (!haddr)
+		return -1;
+
+	arm64_kvm_hyp_debug_uart_addr = haddr;
+
+	return 0;
+}
+#else
+static int create_hyp_debug_uart_mapping(void) { return 0; }
+#endif
+
 static void *vmemmap_base;
 static void *shadow_table_base;
 static void *hyp_pgt_base;
@@ -129,6 +148,10 @@ static int recreate_hyp_mappings(phys_addr_t phys, unsigned long size,
 	prot = pkvm_mkstate(PAGE_HYP_RO, PKVM_PAGE_SHARED_OWNED);
 	ret = pkvm_create_mappings(&kvm_vgic_global_state,
 				   &kvm_vgic_global_state + 1, prot);
+	if (ret)
+		return ret;
+
+	ret = create_hyp_debug_uart_mapping();
 	if (ret)
 		return ret;
 
