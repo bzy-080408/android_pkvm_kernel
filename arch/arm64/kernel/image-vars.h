78464e334219b371d8add723b120bc7e9fa4976f
David Brazdil <dbrazdil@google.com>
2020-04-23 10:40:51 +0100

    arm64: kvm: Split hyp/debug-sr.c to VHE/nVHE
    This patch is part of a series which builds KVM's non-VHE hyp code separately
    from VHE and the rest of the kernel.
    
    debug-sr.c contains KVM's code for context-switching debug registers, with some
    parts shared between VHE/nVHE. These common routines are moved to debug-sr.h,
    VHE-specific code is left in debug-sr.c and nVHE-specific code is moved to
    nvhe/debug-sr.c.
    
    Functions are slightly refactored to move code hidden behind `has_vhe()` checks
    to the corresponding .c files.
    
    Signed-off-by: David Brazdil <dbrazdil@google.com>

--- arch/arm64/kernel/image-vars.h
+++ arch/arm64/kernel/image-vars.h
@@ -61,8 +61,6 @@ __efistub__ctype		= _ctype;
  * memory mappings.
  */
 
-__kvm_nvhe___debug_switch_to_guest = __debug_switch_to_guest;
-__kvm_nvhe___debug_switch_to_host = __debug_switch_to_host;
 __kvm_nvhe___fpsimd_restore_state = __fpsimd_restore_state;
 __kvm_nvhe___fpsimd_save_state = __fpsimd_save_state;
 __kvm_nvhe___guest_enter = __guest_enter;
@@ -71,7 +69,6 @@ __kvm_nvhe___hyp_panic_string = __hyp_panic_string;
 __kvm_nvhe___hyp_stub_vectors = __hyp_stub_vectors;
 __kvm_nvhe___icache_flags = __icache_flags;
 __kvm_nvhe___kvm_enable_ssbs = __kvm_enable_ssbs;
-__kvm_nvhe___kvm_get_mdcr_el2 = __kvm_get_mdcr_el2;
 __kvm_nvhe___kvm_timer_set_cntvoff = __kvm_timer_set_cntvoff;
 __kvm_nvhe___sysreg32_restore_state = __sysreg32_restore_state;
 __kvm_nvhe___sysreg32_save_state = __sysreg32_save_state;
@@ -101,7 +98,6 @@ __kvm_nvhe_cpu_hwcaps = cpu_hwcaps;
 #ifdef CONFIG_ARM64_PSEUDO_NMI
 __kvm_nvhe_gic_pmr_sync = gic_pmr_sync;
 #endif
-__kvm_nvhe_hyp_panic = hyp_panic;
 __kvm_nvhe_idmap_t0sz = idmap_t0sz;
 __kvm_nvhe_kimage_voffset = kimage_voffset;
 __kvm_nvhe_kvm_host_data = kvm_host_data;
