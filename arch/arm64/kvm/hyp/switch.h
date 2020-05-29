378182d397fc241963481e1c45ba4e1853065d0a
David Brazdil <dbrazdil@google.com>
2020-05-29 11:11:56 +0100

    kvm: arm64: Remove __hyp_this_cpu_read
    Similar to adr/ldr_this_cpu, __this_cpu_read and its __my_cpu_offset helper
    would choose between TPIDR_EL1/2 as percpu offset based on nVHE/VHE. When
    running in hyp mode, this needs to always select TPIDR_EL2.
    __hyp_this_cpu_read was used instead for this purpose.
    
    Leverage the __KVM_NVHE_HYPERVISOR__ macro to select the correct register
    in nVHE hyp mode. This will in turn make it possible to use __this_cpu_*
    routines in hyp.
    
    Since these perform a preemption check when compiled with CONFIG_DEBUG_PREEMPT,
    implement a dummy callback function in nVHE hyp to satisfy the dependency.
    This is in line with the original __hyp_this_cpu_read implementation which
    did not perform any checks either.
    
    this_cpu_* routines are not supported in hyp because they toggle
    preemption/interrupts.
    
    Signed-off-by: David Brazdil <dbrazdil@google.com>

--- arch/arm64/kvm/hyp/switch.h
+++ arch/arm64/kvm/hyp/switch.h
@@ -375,7 +375,7 @@ static inline bool __hyp_handle_ptrauth(struct kvm_vcpu *vcpu)
 	    !esr_is_ptrauth_trap(kvm_vcpu_get_hsr(vcpu)))
 		return false;
 
-	ctxt = &__hyp_this_cpu_ptr(kvm_host_data)->host_ctxt;
+	ctxt = &this_cpu_ptr(&kvm_host_data)->host_ctxt;
 	__ptrauth_save_key(ctxt->sys_regs, APIA);
 	__ptrauth_save_key(ctxt->sys_regs, APIB);
 	__ptrauth_save_key(ctxt->sys_regs, APDA);
@@ -484,7 +484,7 @@ static inline void __set_guest_arch_workaround_state(struct kvm_vcpu *vcpu)
 	 * guest wants it disabled, so be it...
 	 */
 	if (__needs_ssbd_off(vcpu) &&
-	    __hyp_this_cpu_read(arm64_ssbd_callback_required))
+	    __this_cpu_read(arm64_ssbd_callback_required))
 		arm_smccc_1_1_smc(ARM_SMCCC_ARCH_WORKAROUND_2, 0, NULL);
 #endif
 }
@@ -496,7 +496,7 @@ static inline void __set_host_arch_workaround_state(struct kvm_vcpu *vcpu)
 	 * If the guest has disabled the workaround, bring it back on.
 	 */
 	if (__needs_ssbd_off(vcpu) &&
-	    __hyp_this_cpu_read(arm64_ssbd_callback_required))
+	    __this_cpu_read(arm64_ssbd_callback_required))
 		arm_smccc_1_1_smc(ARM_SMCCC_ARCH_WORKAROUND_2, 1, NULL);
 #endif
 }
