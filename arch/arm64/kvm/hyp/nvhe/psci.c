81e4a87de1136aaec09a5435b030b528aea5e18b
David Brazdil <dbrazdil@google.com>
2020-07-09 13:00:58 +0200

    Add CPU_OFF handler
    Signed-off-by: David Brazdil <dbrazdil@google.com>

--- arch/arm64/kvm/hyp/nvhe/psci.c
+++ arch/arm64/kvm/hyp/nvhe/psci.c
@@ -12,6 +12,14 @@
 
 #include <uapi/linux/psci.h>
 
+int kvm_host_psci_cpu_off(void)
+{
+	struct arm_smccc_res res;
+	arm_smccc_1_1_smc(PSCI_0_2_FN_CPU_OFF, &res);
+	/* XXX - check that res.a0 is DENIED */
+	return PSCI_RET_DENIED;
+}
+
 int kvm_host_psci_call(struct kvm_vcpu *host_vcpu)
 {
 	bool is_32bit;
@@ -25,6 +33,12 @@ int kvm_host_psci_call(struct kvm_vcpu *host_vcpu)
 		is_32bit = false;
 	else
 		return ret;
+
+	switch (fn_id) {
+	case PSCI_0_2_FN_CPU_OFF:
+		ret = kvm_host_psci_cpu_off();
+		break;
+	}
 
 	return ret;
 }
