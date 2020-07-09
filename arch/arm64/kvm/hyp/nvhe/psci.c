52ad08f5d0b98b7d22971e8a4354ae0ec01d421a
David Brazdil <dbrazdil@google.com>
2020-07-09 12:17:10 +0200

    Add empty PSCI handler
    Signed-off-by: David Brazdil <dbrazdil@google.com>

--- arch/arm64/kvm/hyp/nvhe/psci.c
+++ arch/arm64/kvm/hyp/nvhe/psci.c
@@ -8,9 +8,23 @@
 #include <asm/kvm_hyp.h>
 #include <asm/kvm_mmu.h>
 
+#include <kvm/arm_hypercalls.h>
+
 #include <uapi/linux/psci.h>
 
 int kvm_host_psci_call(struct kvm_vcpu *host_vcpu)
 {
-	return -EINVAL;
+	bool is_32bit;
+	int ret = -EINVAL;
+	unsigned long fn_id = smccc_get_function(host_vcpu);
+	unsigned long fn_base = fn_id & ~PSCI_0_2_FN_ID_MASK;
+
+	if (fn_base == PSCI_0_2_FN_BASE)
+		is_32bit = true;
+	else if (fn_base == PSCI_0_2_FN64_BASE)
+		is_32bit = false;
+	else
+		return ret;
+
+	return ret;
 }
