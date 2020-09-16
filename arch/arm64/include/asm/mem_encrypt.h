b6a4ce7f1d3830a428e3a6eb044a1c72bdf51dc7
Will Deacon <willdeacon@google.com>
2020-09-16 17:39:27 +0100

    squash! arm64: Stub out implementation of memory encryption API
    Initialise swiotlb and move guts into its own C file.
    
    Signed-off-by: Will Deacon <willdeacon@google.com>

--- arch/arm64/include/asm/mem_encrypt.h
+++ arch/arm64/include/asm/mem_encrypt.h
@@ -1,24 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/*
- * Implementation of the memory encryption/decryption API.
- *
- * Amusingly, no crypto is actually performed. Rather, we call into the
- * hypervisor component of KVM to expose pages selectively to the host
- * for virtio "DMA" operations. In other words, "encrypted" pages are
- * not accessible to the host, whereas "decrypted" pages are.
- *
- * Author: Will Deacon <will@kernel.org>
- */
 #ifndef __ASM_MEM_ENCRYPT_H
 #define __ASM_MEM_ENCRYPT_H
 
-// TODO: Hook this to figure out if we're using this stuff or not/
-//void __init mem_encrypt_init(void);
+#include <linux/device.h>
 
-static inline bool mem_encrypt_active(void)
-{
-	return true;
-}
+bool mem_encrypt_active(void);
+int set_memory_encrypted(unsigned long addr, int numpages);
+int set_memory_decrypted(unsigned long addr, int numpages);
 
 static inline bool force_dma_unencrypted(struct device *dev)
 {
@@ -26,17 +14,4 @@ static inline bool force_dma_unencrypted(struct device *dev)
 	return true;
 }
 
-static inline int set_memory_encrypted(unsigned long addr, int numpages)
-{
-	// TODO: unshare the page from the host
-	trace_printk("0x%lx - 0x%lx\n", addr, addr + numpages * PAGE_SIZE);
-	return 0;
-}
-
-static inline int set_memory_decrypted(unsigned long addr, int numpages)
-{
-	// TODO: share the page with the host
-	trace_printk("0x%lx - 0x%lx\n", addr, addr + numpages * PAGE_SIZE);
-	return 0;
-}
 #endif	/* __ASM_MEM_ENCRYPT_H */
---
+++ arch/arm64/kernel/mem_encrypt.c
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Implementation of the memory encryption/decryption API.
+ *
+ * Amusingly, no crypto is actually performed. Rather, we call into the
+ * hypervisor component of KVM to expose pages selectively to the host
+ * for virtio "DMA" operations. In other words, "encrypted" pages are
+ * not accessible to the host, whereas "decrypted" pages are.
+ *
+ * Author: Will Deacon <will@kernel.org>
+ */
+
+#include <linux/kernel.h>
+#include <linux/mem_encrypt.h>
+#include <linux/swiotlb.h>
+
+int set_memory_encrypted(unsigned long addr, int numpages)
+{
+	/* TODO: unshare the page from the host */
+	trace_printk("0x%lx - 0x%lx\n", addr, addr + numpages * PAGE_SIZE);
+	dump_stack();
+	return 0;
+}
+
+int set_memory_decrypted(unsigned long addr, int numpages)
+{
+	/* TODO: share the page with the host */
+	trace_printk("0x%lx - 0x%lx\n", addr, addr + numpages * PAGE_SIZE);
+	dump_stack();
+	return 0;
+}
+
+bool mem_encrypt_active(void)
+{
+	/* TODO: Probe from KVM and returned cached result here */
+	return true;
+}
+
+void __init mem_encrypt_init(void)
+{
+	swiotlb_update_mem_attributes();
+}
