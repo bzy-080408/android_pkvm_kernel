86b8546a280176ebbcde02458a1bbfd196c17d10
Andrew Walbran <qwandor@google.com>
2021-04-28 12:55:01 +0000

    KVM: arm64: Handle FFA_RXTX_MAP.
    This maps the FF-A RX and TX buffers into the hypervisor, ready to
    intercept subsequent messages. Note that this means that any FF-A calls
    involving the message buffers will fail until they are implemented in the
    hypervisor, as passing them through to the SPM will no longer work.
    
    For now this only supports a mailbox buffer size of 4 KiB, and assumes
    that the stage-2 page table also uses 4 KiB pages.
    
    Signed-off-by: Andrew Walbran <qwandor@google.com>

--- arch/arm64/kvm/hyp/nvhe/ffa_handler.c
+++ arch/arm64/kvm/hyp/nvhe/ffa_handler.c
@@ -4,14 +4,27 @@
  * Author: Andrew Walbran <qwandor@google.com>
  */
 
+#include <asm/kvm_pgtable.h>
 #include <linux/arm-smccc.h>
 #include <linux/arm_ffa.h>
+#include <linux/kvm_types.h>
 #include <nvhe/ffa.h>
 #include <nvhe/ffa_handler.h>
+#include <nvhe/mem_protect.h>
+#include <nvhe/mm.h>
+#include <nvhe/spinlock.h>
 #include <nvhe/trap_handler.h>
+#include <linux/align.h>
 
 // TODO: Initialise this properly
 u64 __ro_after_init smccc_has_sve_hint;
+
+/*
+ * The size of RX/TX buffer which we support. The implementation assumes that
+ * this is the same size as the stage 2 page table page size.
+ */
+// TODO: Support other page sizes before this goes upstream.
+#define MAILBOX_SIZE 4096
 
 /** Constructs an FF-A error return value with the specified error code. */
 static struct arm_smccc_1_2_regs ffa_error(u64 error_code)
@@ -40,6 +53,88 @@ static struct arm_smccc_1_2_regs ffa_version(u32 input_version)
 	};
 }
 
+/**
+ * ffa_rxtx_map() - Handles the FFA_RXTX_MAP function.
+ * @tx_address: The IPA of the TX buffer.
+ * @rx_address: The IPA of the RX buffer.
+ * @page_count: The length of the RX and TX buffers, as a multiple of
+ *              FFA_PAGE_SIZE.
+ *
+ * Return: FFA_SUCCESS, or an appropriate FFA_ERROR.
+ */
+static struct arm_smccc_1_2_regs ffa_rxtx_map(hpa_t tx_address,
+					      hpa_t rx_address, u32 page_count)
+{
+	struct arm_smccc_1_2_regs ret;
+
+	/* We only support a fixed size of RX/TX buffers. */
+	if (page_count != MAILBOX_SIZE / FFA_PAGE_SIZE)
+		return ffa_error(FFA_RET_INVALID_PARAMETERS);
+
+	/* Fail if addresses are not page-aligned. */
+	if (!IS_ALIGNED(tx_address, PAGE_SIZE) ||
+	    !IS_ALIGNED(rx_address, PAGE_SIZE)) {
+		return ffa_error(FFA_RET_INVALID_PARAMETERS);
+	}
+
+	/* Fail if the same page is used for the send and receive pages. */
+	if (tx_address == rx_address)
+		return ffa_error(FFA_RET_INVALID_PARAMETERS);
+
+	/* Lock host VM info and hypervisor page table. */
+	hyp_spin_lock(&host_kvm.lock);
+	hyp_spin_lock(&pkvm_pgd_lock);
+
+	/* Ensure that buffers are not already setup. */
+	if (host_kvm.tx_buffer != NULL || host_kvm.rx_buffer != NULL) {
+		ret = ffa_error(FFA_RET_DENIED);
+		goto out;
+	}
+
+	// TODO: Check that MAILBOX_SIZE == page size
+	/*
+	 * Ensure that the VM is allowed to share the pages, i.e. it exclusively
+	 * owns them.
+	 */
+	if (__pkvm_host_check_share_hyp_prot(tx_address, MAILBOX_SIZE,
+					     PAGE_HYP_RO) != 0) {
+		ret = ffa_error(FFA_RET_DENIED);
+		goto out;
+	}
+	if (__pkvm_host_check_share_hyp_prot(rx_address, MAILBOX_SIZE,
+					     PAGE_HYP) != 0) {
+		ret = ffa_error(FFA_RET_DENIED);
+		goto out;
+	}
+
+	/*
+	 * Mark as no longer exclusive to the VM, and map in the hypervisor
+	 * stage-1 page table. This takes pkvm_pgd_lock when needed.
+	 */
+	if (__pkvm_host_share_hyp_prot(tx_address, MAILBOX_SIZE, PAGE_HYP_RO,
+				       &host_kvm.tx_buffer) != 0) {
+		ret = ffa_error(FFA_RET_NO_MEMORY);
+		goto out;
+	}
+	if (__pkvm_host_share_hyp_prot(rx_address, MAILBOX_SIZE, PAGE_HYP,
+				       &host_kvm.rx_buffer) != 0) {
+		ret = ffa_error(FFA_RET_NO_MEMORY);
+		goto out;
+	}
+
+	// TODO: Do we need to do something about waiters?
+
+	ret = (struct arm_smccc_1_2_regs){
+		.a0 = FFA_SUCCESS,
+	};
+
+out:
+	hyp_spin_unlock(&host_kvm.lock);
+	hyp_spin_unlock(&pkvm_pgd_lock);
+
+	return ret;
+}
+
 bool kvm_host_ffa_handler(struct kvm_cpu_context *host_ctxt)
 {
 	struct arm_smccc_1_2_regs ret;
@@ -63,11 +158,11 @@ bool kvm_host_ffa_handler(struct kvm_cpu_context *host_ctxt)
 	case FFA_RX_RELEASE:
 	case FFA_PARTITION_INFO_GET:
 	case FFA_ID_GET:
-	case FFA_MSG_POLL:
-	case FFA_MSG_WAIT:
+	case FFA_MSG_POLL: // TODO: Need to copy buffer on return
+	case FFA_MSG_WAIT: // TODO: Need to copy buffer on return
 	case FFA_YIELD:
 	case FFA_RUN:
-	case FFA_MSG_SEND:
+	case FFA_MSG_SEND: // TODO: Need to copy buffer
 	case FFA_MSG_SEND_DIRECT_REQ:
 	case FFA_FN64_MSG_SEND_DIRECT_REQ:
 	case FFA_MSG_SEND_DIRECT_RESP:
@@ -88,7 +183,10 @@ bool kvm_host_ffa_handler(struct kvm_cpu_context *host_ctxt)
 	case FFA_VERSION:
 		ret = ffa_version(a1);
 		break;
-	case FFA_RXTX_MAP:
+	case FFA_RXTX_MAP: // Should this be allowed?
+	case FFA_FN64_RXTX_MAP:
+		ret = ffa_rxtx_map(a1, a2, a3);
+		break;
 	case FFA_RXTX_UNMAP:
 	case FFA_MEM_DONATE:
 	case FFA_FN64_MEM_DONATE:
