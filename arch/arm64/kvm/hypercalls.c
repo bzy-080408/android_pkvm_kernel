0dac3b4f406b9eaaf56108f6f0c09220815a596a
Marc Zyngier <maz@kernel.org>
2021-06-21 19:49:45 +0100

    KVM: arm64: pkvm: Wire MMIO guard hypercalls
    Plumb in the hypercall interface to allow a guest to discover,
    enroll, map and unmap MMIO regions.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/kvm/hypercalls.c
+++ arch/arm64/kvm/hypercalls.c
@@ -5,6 +5,7 @@
 #include <linux/kvm_host.h>
 
 #include <asm/kvm_emulate.h>
+#include <asm/stage2_pgtable.h>
 
 #include <kvm/arm_hypercalls.h>
 #include <kvm/arm_psci.h>
@@ -212,6 +213,10 @@ int kvm_hvc_call_handler(struct kvm_vcpu *vcpu)
 		break;
 	case ARM_SMCCC_VENDOR_HYP_KVM_PTP_FUNC_ID:
 		kvm_ptp_get_time(vcpu, val);
+		break;
+	case ARM_SMCCC_VENDOR_HYP_KVM_MMIO_GUARD_MAP_FUNC_ID:
+		if (kvm_vm_is_protected(vcpu->kvm) && !topup_hyp_memcache(vcpu))
+			val[0] = SMCCC_RET_SUCCESS;
 		break;
 	case ARM_SMCCC_TRNG_VERSION:
 	case ARM_SMCCC_TRNG_FEATURES:
