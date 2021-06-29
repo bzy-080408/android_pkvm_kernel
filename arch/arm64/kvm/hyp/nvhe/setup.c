5c5cc0c3d12b0c8e1ee6e156da3eb804ec8221e3
David Brazdil <dbrazdil@google.com>
2021-06-29 22:05:26 +0000

    ANDROID: KVM: arm64: Introduce IOMMU driver infrastructure
    Bootstrap infrastructure for IOMMU drivers by introducing kvm_iommu_ops
    struct in EL2 that is populated based on a iommu_driver parameter to
    __pkvm_init hypercall and selected in EL1 early init.
    
    An 'init' operation is called in __pkvm_init_finalise, giving the driver
    an opportunity to initialize itself in EL2 and create any EL2 mappings
    that it will need. 'init' is specifically called before
    'finalize_host_mappings' so that:
      (a) pages mapped by the driver change owner to hyp,
      (b) ownership changes in 'finalize_host_mappings' get reflected in
          IOMMU mappings (added in a future patch).
    
    Test: builds, boots
    Bug: 190463801
    Signed-off-by: David Brazdil <dbrazdil@google.com>
    Change-Id: Icd496d193c0bbf811406c3a8d4b94610e2a03197

--- arch/arm64/kvm/hyp/nvhe/setup.c
+++ arch/arm64/kvm/hyp/nvhe/setup.c
@@ -211,6 +211,16 @@ static int finalize_host_mappings(void)
 	return kvm_pgtable_walk(&pkvm_pgtable, 0, BIT(pkvm_pgtable.ia_bits), &walker);
 }
 
+static int select_iommu_ops(enum kvm_iommu_driver driver)
+{
+	switch (driver) {
+	case KVM_IOMMU_DRIVER_NONE:
+		return 0;
+	}
+
+	return -EINVAL;
+}
+
 void __noreturn __pkvm_init_finalise(void)
 {
 	struct kvm_host_data *host_data = this_cpu_ptr(&kvm_host_data);
@@ -230,10 +240,6 @@ void __noreturn __pkvm_init_finalise(void)
 	if (ret)
 		goto out;
 
-	ret = finalize_host_mappings();
-	if (ret)
-		goto out;
-
 	pkvm_pgtable_mm_ops = (struct kvm_pgtable_mm_ops) {
 		.zalloc_page = hyp_zalloc_hyp_page,
 		.phys_to_virt = hyp_phys_to_virt,
@@ -242,6 +248,16 @@ void __noreturn __pkvm_init_finalise(void)
 		.put_page = hpool_put_page,
 	};
 	pkvm_pgtable.mm_ops = &pkvm_pgtable_mm_ops;
+
+	if (kvm_iommu_ops.init) {
+		ret = kvm_iommu_ops.init();
+		if (ret)
+			goto out;
+	}
+
+	ret = finalize_host_mappings();
+	if (ret)
+		goto out;
 
 out:
 	/*
@@ -254,7 +270,8 @@ out:
 }
 
 int __pkvm_init(phys_addr_t phys, unsigned long size, unsigned long nr_cpus,
-		unsigned long *per_cpu_base, u32 hyp_va_bits)
+		unsigned long *per_cpu_base, u32 hyp_va_bits,
+		enum kvm_iommu_driver iommu_driver)
 {
 	struct kvm_nvhe_init_params *params;
 	void *virt = hyp_phys_to_virt(phys);
@@ -274,6 +291,10 @@ int __pkvm_init(phys_addr_t phys, unsigned long size, unsigned long nr_cpus,
 		return ret;
 
 	ret = recreate_hyp_mappings(phys, size, per_cpu_base, hyp_va_bits);
+	if (ret)
+		return ret;
+
+	ret = select_iommu_ops(iommu_driver);
 	if (ret)
 		return ret;
 
