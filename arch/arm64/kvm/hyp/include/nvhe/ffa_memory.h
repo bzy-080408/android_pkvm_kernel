c9c7727b6a86181dc8694989b1702a1a63e05078
Andrew Walbran <qwandor@google.com>
2021-10-26 10:50:08 +0000

    KVM: arm64: Handle FF-A memory sharing to secure world.
    This includes FFA_MEM_DONATE, FFA_MEM_LEND and FFA_MEM_SHARE.
    
    Signed-off-by: Andrew Walbran <qwandor@google.com>

---
+++ arch/arm64/kvm/hyp/include/nvhe/ffa_memory.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2021 - Google LLC
+ * Author: Andrew Walbran <qwandor@google.com>
+ */
+
+#include <linux/arm-smccc.h>
+
+struct hyp_pool;
+
+struct arm_smccc_1_2_regs
+ffa_memory_tee_send(struct kvm_pgtable *from_pgt,
+		    struct ffa_mem_region *memory_region,
+		    uint32_t memory_share_length, uint32_t fragment_length,
+		    uint32_t share_func, struct hyp_pool *page_pool);
