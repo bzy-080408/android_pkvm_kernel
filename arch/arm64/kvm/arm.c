cc290b9ed456cad46197251c257d6ed7feb0e9af
Quentin Perret <qperret@google.com>
2021-11-03 15:23:13 +0000

    ANDROID: KVM: arm64: Reclaim guest page-table pages during teardown
    The memory pages donated by the host to the hypervisor for the creation
    of guest page-tables is currently never reclaimed. Fix this returning
    those pages back to the host during guest teardown using a hyp_memcache.
    
    Signed-off-by: Quentin Perret <qperret@google.com>
    Bug: 209580772
    Change-Id: Iaa327da9f594daaed50a9fcc319768b0230e9531

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -161,6 +161,7 @@ vm_fault_t kvm_arch_vcpu_fault(struct kvm_vcpu *vcpu, struct vm_fault *vmf)
 	return VM_FAULT_SIGBUS;
 }
 
+void free_hyp_memcache(struct kvm_hyp_memcache *mc);
 static void kvm_shadow_destroy(struct kvm *kvm)
 {
 	struct arm_smccc_res res;
@@ -171,6 +172,8 @@ static void kvm_shadow_destroy(struct kvm *kvm)
 	arm_smccc_1_1_hvc(KVM_HOST_SMCCC_FUNC(__pkvm_teardown_shadow),
 			kvm, &res);
 	WARN_ON(res.a0 != SMCCC_RET_SUCCESS);
+
+	free_hyp_memcache(&kvm->arch.pkvm.teardown_mc);
 }
 
 /**
