b136cd03950f91a87905df2f77cfd6ce5ced66e7
Fuad Tabba <tabba@google.com>
2022-03-31 18:36:39 +0100

    KVM: arm64: Introduce shadow VM state at EL2
    Introduce a table of shadow VM structures at EL2 and provide hypercalls
    to the host for creating and destroying shadow VMs.
    
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Signed-off-by: Will Deacon <will@kernel.org>

---
+++ arch/arm64/kvm/hyp/include/nvhe/pkvm.h
@@ -0,0 +1,49 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2021 Google LLC
+ * Author: Fuad Tabba <tabba@google.com>
+ */
+
+#ifndef __ARM64_KVM_NVHE_PKVM_H__
+#define __ARM64_KVM_NVHE_PKVM_H__
+
+#include <asm/kvm_pkvm.h>
+
+/*
+ * Holds the relevant data for maintaining the vcpu state completely at hyp.
+ */
+struct kvm_shadow_vcpu_state {
+	struct kvm_vcpu shadow_vcpu;
+	struct kvm_vcpu *host_vcpu;
+	struct kvm_shadow_vm *shadow_vm;
+};
+
+/*
+ * Holds the relevant data for running a protected vm.
+ */
+struct kvm_shadow_vm {
+	struct kvm kvm;
+	struct kvm *host_kvm;
+	size_t shadow_area_size;
+	struct kvm_pgtable pgt;
+
+	/* Array containing the shadow state for each vcpu. */
+	struct kvm_shadow_vcpu_state shadow_vcpu_states[0];
+};
+
+static inline struct kvm_shadow_vcpu_state *get_shadow_state(struct kvm_vcpu *shadow_vcpu)
+{
+	return container_of(shadow_vcpu, struct kvm_shadow_vcpu_state, shadow_vcpu);
+}
+
+static inline struct kvm_shadow_vm *get_shadow_vm(struct kvm_vcpu *shadow_vcpu)
+{
+	return get_shadow_state(shadow_vcpu)->shadow_vm;
+}
+
+void hyp_shadow_table_init(void *tbl);
+int __pkvm_init_shadow(struct kvm *kvm, unsigned long shadow_hva,
+		       size_t shadow_size, unsigned long pgd_hva);
+int __pkvm_teardown_shadow(unsigned int shadow_handle);
+
+#endif /* __ARM64_KVM_NVHE_PKVM_H__ */
