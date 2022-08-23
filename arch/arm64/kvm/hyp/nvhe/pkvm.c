c1c222bba170802f8371aec3aa346362bb21f7d8
Fuad Tabba <tabba@google.com>
2022-08-23 10:47:54 +0100

    fixup! KVM: arm64: Introduce shadow VM state at EL2
    KVM: arm64: Refactor unpinning host vcpu
    
    Factor out unpinning a single host vcpu from unpin_host_vcpus(),
    since it will be used in a future patch in the error path.
    
    No functional change intended.
    
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/kvm/hyp/nvhe/pkvm.c
+++ arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -235,16 +235,21 @@ static struct kvm_shadow_vm *find_shadow_by_handle(unsigned int shadow_handle)
 	return shadow_table[shadow_idx];
 }
 
+static void unpin_host_vcpu(struct kvm_shadow_vcpu_state *shadow_vcpu_state)
+{
+	struct kvm_vcpu *host_vcpu = shadow_vcpu_state->host_vcpu;
+
+	if (host_vcpu)
+		hyp_unpin_shared_mem(host_vcpu, host_vcpu + 1);
+}
+
 static void unpin_host_vcpus(struct kvm_shadow_vcpu_state *shadow_vcpu_states,
 			     unsigned int nr_vcpus)
 {
 	int i;
 
-	for (i = 0; i < nr_vcpus; i++) {
-		struct kvm_vcpu *host_vcpu = shadow_vcpu_states[i].host_vcpu;
-
-		hyp_unpin_shared_mem(host_vcpu, host_vcpu + 1);
-	}
+	for (i = 0; i < nr_vcpus; i++)
+		unpin_host_vcpu(&shadow_vcpu_states[i]);
 }
 
 static int set_host_vcpus(struct kvm_shadow_vcpu_state *shadow_vcpu_states,
