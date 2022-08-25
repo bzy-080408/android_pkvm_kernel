c83320f5b6fe1604c19fb9c48913b1a2fc751edd
Vincent Donnefort <vdonnefort@google.com>
2022-08-25 10:33:32 +0100

    KVM: arm64: Add tracing support for the nVHE hypervisor
    Running at EL2, the host has very close to no way to know what's happening
    in the hypervisor, which is great from a security point of view, a bit less
    when it turns to debug it. With the introduction of the protected mode,
    this piece of code is getting more responsibilities, which we would like to
    debug and profile, hence the need to trace "things" that are happening in
    the hypervisor.
    
    There's no way the hypervisor could log things directly into the host
    tracing interface. So instead let's use a separated per-CPU ring buffer
    compliant with the host so the latter can decode the events.
    
    The tracing interface is composed of 4 HVCs:
    
     __pkvm_start_tracing: Gets a hyp_trace_pack describing the ring buffers,
     the backing storage that'll support the internal structures and some clock
     init values.
    
     __pkvm_stop_tracing: Disable writing to the ring buffers and teardown.
    
     __pkvm_rb_swap_reader_page: Request the writer to swap the head with
     the reader page. This enables consuming read of the read buffer. See
     kernel/ring_buffer.c (*ext_writer_swap_reader)() callback.
    
     __pkvm_rb_update_footers: Request the writer to update the pages footers.
     Those informations are then used to update the reader view on the ring
     buffer. See kernel/ring_buffer.c (*ext_writer_update_footers)() callback.
    
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/hyp-main.c
+++ arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -20,6 +20,7 @@
 #include <nvhe/mem_protect.h>
 #include <nvhe/mm.h>
 #include <nvhe/pkvm.h>
+#include <nvhe/trace.h>
 #include <nvhe/trap_handler.h>
 
 #include <linux/irqchip/arm-gic-v3.h>
@@ -1117,6 +1118,35 @@ static void handle___pkvm_iommu_finalize(struct kvm_cpu_context *host_ctxt)
 	cpu_reg(host_ctxt, 1) = __pkvm_iommu_finalize();
 }
 
+static void handle___pkvm_start_tracing(struct kvm_cpu_context *host_ctxt)
+{
+	 DECLARE_REG(unsigned long, pack_hva, host_ctxt, 1);
+	 DECLARE_REG(size_t, pack_size, host_ctxt, 2);
+
+	 cpu_reg(host_ctxt, 1) = __pkvm_start_tracing(pack_hva, pack_size);
+}
+
+static void handle___pkvm_stop_tracing(struct kvm_cpu_context *host_ctxt)
+{
+	__pkvm_stop_tracing();
+
+	cpu_reg(host_ctxt, 1) = 0;
+}
+
+static void handle___pkvm_rb_swap_reader_page(struct kvm_cpu_context *host_ctxt)
+{
+	DECLARE_REG(int, cpu, host_ctxt, 1);
+
+	cpu_reg(host_ctxt, 1) = __pkvm_rb_swap_reader_page(cpu);
+}
+
+static void handle___pkvm_rb_update_footers(struct kvm_cpu_context *host_ctxt)
+{
+	DECLARE_REG(int, cpu, host_ctxt, 1);
+
+	cpu_reg(host_ctxt, 1) = __pkvm_rb_update_footers(cpu);
+}
+
 typedef void (*hcall_t)(struct kvm_cpu_context *);
 
 #define HANDLE_FUNC(x)	[__KVM_HOST_SMCCC_FUNC_##x] = (hcall_t)handle_##x
@@ -1154,6 +1184,10 @@ static const hcall_t host_hcall[] = {
 	HANDLE_FUNC(__pkvm_iommu_register),
 	HANDLE_FUNC(__pkvm_iommu_pm_notify),
 	HANDLE_FUNC(__pkvm_iommu_finalize),
+	HANDLE_FUNC(__pkvm_start_tracing),
+	HANDLE_FUNC(__pkvm_stop_tracing),
+	HANDLE_FUNC(__pkvm_rb_swap_reader_page),
+	HANDLE_FUNC(__pkvm_rb_update_footers),
 };
 
 static inline u64 kernel__text_addr(void)
