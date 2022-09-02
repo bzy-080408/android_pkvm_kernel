18cdf1eb7b5d74cca907597547d6eee899e77161
Vincent Donnefort <vdonnefort@google.com>
2022-09-02 11:13:36 +0100

    ANDROID: KVM: arm64: Use static branch for nVHE hyp events
    Now the nVHE hypervisor supports toggling static branches, protect nVHE
    events with one. This intends to reduce the cost of event tracing when
    disabled.
    
    Bug: 229972309
    Change-Id: I79b191dc830bff50985cd4aab9fb61325c1f7e6f
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/trace.h
+++ arch/arm64/kvm/hyp/include/nvhe/trace.h
@@ -66,7 +66,7 @@ int __pkvm_enable_event(unsigned short id, bool enable);
 
 #define HYP_EVENT(__name, __proto, __struct, __assign, __printk)		\
 	HYP_EVENT_FORMAT(__name, __struct);					\
-	extern atomic_t __name##_enabled;					\
+	DECLARE_STATIC_KEY_FALSE(__name##_enabled);				\
 	extern unsigned short hyp_event_id_##__name;				\
 	static inline void trace_hyp_##__name(__proto)				\
 	{									\
@@ -74,7 +74,7 @@ int __pkvm_enable_event(unsigned short id, bool enable);
 		struct hyp_rb_per_cpu *rb = this_cpu_ptr(&trace_rb);		\
 		struct trace_hyp_format_##__name *__entry;			\
 										\
-		if (!atomic_read(&__name##_enabled))				\
+		if (!static_branch_unlikely(&__name##_enabled))			\
 			return;							\
 		if (!__start_write_hyp_rb(rb))					\
 			return;							\
@@ -83,8 +83,6 @@ int __pkvm_enable_event(unsigned short id, bool enable);
 		__assign							\
 		__stop_write_hyp_rb(rb);					\
 	}
-
-/* TODO: atomic_t to static_branch */
 
 #else
 static inline int __pkvm_start_tracing(unsigned long pack_va, size_t pack_size)
--- arch/arm64/kvm/hyp/nvhe/events.c
+++ arch/arm64/kvm/hyp/nvhe/events.c
@@ -5,7 +5,7 @@ extern struct hyp_event_id __hyp_event_ids_end[];
 
 #undef HYP_EVENT
 #define HYP_EVENT(__name, __proto, __struct, __assign, __printk)	\
-	atomic_t __name##_enabled = ATOMIC_INIT(0);			\
+	DEFINE_STATIC_KEY_FALSE(__name##_enabled);			\
 	struct hyp_event_id hyp_event_id_##__name __section("_hyp_event_ids") = {	\
 		.data = (void *)&__name##_enabled,			\
 	}
@@ -15,19 +15,19 @@ extern struct hyp_event_id __hyp_event_ids_end[];
 int __pkvm_enable_event(unsigned short id, bool enable)
 {
 	struct hyp_event_id *event_id = __hyp_event_ids_start;
-	atomic_t *enable_key;
+	struct static_key_false *enable_key;
 
 	for (; (unsigned long)event_id < (unsigned long)__hyp_event_ids_end;
 	     event_id++) {
 		if (event_id->id != id)
 			continue;
 
-		enable_key = (atomic_t *)event_id->data;
+		enable_key = (struct static_key_false *)event_id->data;
 
 		if (enable)
-			atomic_set(enable_key, 1);
+			static_branch_enable(enable_key);
 		else
-			atomic_set(enable_key, 0);
+			static_branch_disable(enable_key);
 
 		return 0;
 	}
