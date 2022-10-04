e32a6ed04d0e607ff05b8b897b2c6a091d40cde8
Vincent Donnefort <vdonnefort@google.com>
2022-10-04 11:37:48 +0100

    ANDROID: KVM: arm64: Fix pKVM module load with CFI
    Give a non-CFI value (function_nocfi()) of the module EL2 init function.
    This mandates the use of a macro. function_nocfi() can't use function
    pointers.
    
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/include/asm/kvm_pkvm_module.h
+++ arch/arm64/include/asm/kvm_pkvm_module.h
@@ -25,13 +25,45 @@ struct pkvm_el2_module {
 	int (*init)(const struct pkvm_module_ops *ops);
 };
 
-int __pkvm_load_el2_module(struct pkvm_el2_module *mod, struct module *this);
-static __always_inline int pkvm_load_el2_module(struct pkvm_el2_module *mod)
-{
-	return __pkvm_load_el2_module(mod, THIS_MODULE);
-}
-
 #ifdef MODULE
+int __pkvm_load_el2_module(struct pkvm_el2_module *mod, struct module *this);
+
+/**
+ * pkvm_load_el2_module: blah
+ *
+ * function_nocfi() does not work with function pointers, hence the macro in
+ * lieu of a function.
+ */
+#define pkvm_load_el2_module(init_fn)					\
+({									\
+	extern char __kvm_nvhe___hypmod_text_start[];			\
+	extern char __kvm_nvhe___hypmod_text_end[];			\
+	extern char __kvm_nvhe___hypmod_bss_start[];			\
+	extern char __kvm_nvhe___hypmod_bss_end[];			\
+	extern char __kvm_nvhe___hypmod_rodata_start[];			\
+	extern char __kvm_nvhe___hypmod_rodata_end[];			\
+	extern char __kvm_nvhe___hypmod_data_start[];			\
+	extern char __kvm_nvhe___hypmod_data_end[];			\
+	extern char __kvm_nvhe___hyprel_start[];			\
+	extern char __kvm_nvhe___hyprel_end[];				\
+	struct pkvm_el2_module mod;					\
+									\
+	mod.text.start		= __kvm_nvhe___hypmod_text_start;	\
+	mod.text.end		= __kvm_nvhe___hypmod_text_end;		\
+	mod.bss.start		= __kvm_nvhe___hypmod_bss_start;	\
+	mod.bss.end		= __kvm_nvhe___hypmod_bss_end;		\
+	mod.rodata.start	= __kvm_nvhe___hypmod_rodata_start;	\
+	mod.rodata.end		= __kvm_nvhe___hypmod_rodata_end;	\
+	mod.data.start		= __kvm_nvhe___hypmod_data_start;	\
+	mod.data.end		= __kvm_nvhe___hypmod_data_end;		\
+	mod.relocs		= (kvm_nvhe_reloc_t *)__kvm_nvhe___hyprel_start; \
+	mod.nr_relocs		= (__kvm_nvhe___hyprel_end - __kvm_nvhe___hyprel_start) / \
+				  sizeof(*mod.relocs); \
+	mod.init = function_nocfi(init_fn);				\
+									\
+	__pkvm_load_el2_module(&mod, THIS_MODULE);			\
+})
+
 static inline void init_pkvm_module_sections(struct pkvm_el2_module *mod)
 {
 	extern char __kvm_nvhe___hypmod_text_start[];
