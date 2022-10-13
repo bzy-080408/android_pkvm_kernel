baa880b90dc50c1732bcc564ae4583dfa1585b37
Vincent Donnefort <vdonnefort@google.com>
2022-10-13 16:56:24 +0100

    [ DONOTMERGE ] pkvm-pl011: Test hvc registration
    Change-Id: Ibeb94738004aebf0e4d0ff891b4e1601df533d76

--- drivers/misc/pkvm-pl011/pl011-host.c
+++ drivers/misc/pkvm-pl011/pl011-host.c
@@ -5,6 +5,7 @@
 #include <asm/kvm_pkvm_module.h>
 
 int __kvm_nvhe_pl011_hyp_init(const struct pkvm_module_ops *ops);
+void __kvm_nvhe_pl011_hyp_test_hcall(struct kvm_cpu_context *unused);
 
 static int __init pl011_nvhe_init(void)
 {
@@ -13,6 +14,14 @@ static int __init pl011_nvhe_init(void)
 	ret = pkvm_load_el2_module(__kvm_nvhe_pl011_hyp_init);
 	if (ret)
 		return ret;
+
+	ret = pkvm_register_el2_mod_call(__kvm_nvhe_pl011_hyp_test_hcall);
+	if (ret < 0) {
+		printk("Failed to register el2 call\n");
+	} else {
+		printk("Spirit of EL2, are you there? (hcall id=%d)\n", ret);
+		pkvm_el2_mod_call(ret);
+	}
 
 	return 0;
 }
