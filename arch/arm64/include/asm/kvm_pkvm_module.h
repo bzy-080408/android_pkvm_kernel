17eea727393d7c519dec14acffeb780747d027b9
Quentin Perret <qperret@google.com>
2022-10-26 10:32:15 +0000

    ANDROID: KVM: arm64: Expose kvm_flush_dcache_to_poc() in module_ops
    Expose kvm_flush_dcache_to_poc() in the module_ops struct to allow CMOs
    from pKVM modules.
    
    Change-Id: I91d57a94effd2710d868591c6baf4a5672d149a4
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/include/asm/kvm_pkvm_module.h
+++ arch/arm64/include/asm/kvm_pkvm_module.h
@@ -14,6 +14,7 @@ struct pkvm_module_ops {
 	void (*putx64)(u64 num);
 	void *(*fixmap_map)(phys_addr_t phys);
 	int (*fixmap_unmap)(void);
+	void (*flush_dcache_to_poc)(void *addr, size_t size);
 };
 
 struct pkvm_module_section {
