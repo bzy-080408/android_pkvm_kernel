bccc99afe8ee51558cd62b431c93373f030c789a
Fuad Tabba <tabba@google.com>
2022-11-04 09:30:49 +0000

    fixup! KVM: Enable and expose KVM_MEM_PRIVATE
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -1753,7 +1753,8 @@ bool __weak kvm_arch_has_private_mem(struct kvm *kvm)
 	return false;
 }
 
-static int check_memory_region_flags(const struct kvm_user_mem_region *mem)
+static int check_memory_region_flags(struct kvm *kvm,
+				     const struct kvm_user_mem_region *mem)
 {
 	u32 valid_flags = KVM_MEM_LOG_DIRTY_PAGES;
 
@@ -2168,7 +2169,7 @@ int __kvm_set_memory_region(struct kvm *kvm,
 	int as_id, id;
 	int r;
 
-	r = check_memory_region_flags(mem);
+	r = check_memory_region_flags(kvm, mem);
 	if (r)
 		return r;
 
