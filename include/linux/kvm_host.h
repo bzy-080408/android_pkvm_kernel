dff14e91cd2c7e3842ee23565ea569168b655ca3
Fuad Tabba <tabba@google.com>
2022-12-05 12:39:44 +0000

    KVM: expose kvm_vm_ioctl_set_mem_attributes
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -1436,6 +1436,8 @@ long kvm_arch_vm_ioctl(struct file *filp,
 		       unsigned int ioctl, unsigned long arg);
 long kvm_arch_vm_compat_ioctl(struct file *filp, unsigned int ioctl,
 			      unsigned long arg);
+int kvm_vm_ioctl_set_mem_attributes(struct kvm *kvm,
+				    struct kvm_memory_attributes *attrs);
 
 int kvm_arch_vcpu_ioctl_get_fpu(struct kvm_vcpu *vcpu, struct kvm_fpu *fpu);
 int kvm_arch_vcpu_ioctl_set_fpu(struct kvm_vcpu *vcpu, struct kvm_fpu *fpu);
